df22bccdebdc47bf044c971cb0be01b28c107154
sidhb23 <siddhartha2002@hotmail.com>
2024-04-12 10:57:42 -0400

    Updated speed_test.c

--- speed_test.c
+++ speed_test.c
@@ -1,2 +1,52 @@
-// This file will contain code for speed testing
-//Test
+#include <stdio.h>
+#include <curl/curl.h>
+#include <time.h>
+#include <unistd.h>
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    return fwrite(ptr, size, nmemb, stream);
+}
+
+void test_download_speed() {
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    char *url = "https://ash-speed.hetzner.com/100MB.bin";
+    char outfilename[FILENAME_MAX] = "/dev/null"; // Change this on Windows to "NUL"
+    long fileSize;
+    double total_time, speed;
+
+    curl = curl_easy_init();
+    if (curl) {
+        fp = fopen(outfilename,"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        clock_t start = clock();
+        res = curl_easy_perform(curl);
+        clock_t end = clock();
+
+        if (res == CURLE_OK) {
+            curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &fileSize);
+            total_time = (double)(end - start) / CLOCKS_PER_SEC;
+            speed = (fileSize / total_time) / (1024 * 1024); // Speed in Mbps
+            printf("Download speed: %.2f Mbps\n", speed);
+        } else {
+            fprintf(stderr, "Curl perform error: %s\n", curl_easy_strerror(res));
+        }
+
+        fclose(fp);
+        curl_easy_cleanup(curl);
+    }
+}
+
+int main(void) {
+    while(1) {
+        test_download_speed();
+        sleep(1); // Use Sleep(5 * 1000) on Windows
+    }
+    return 0;
+}
